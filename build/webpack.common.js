9f88a5bbbda06cf54a8b442aa6db252169da3431
amipei <156516399@qq.com>
2020-05-08 20:13:29 +0800

    配置基础开发环境

---
+++ build/webpack.common.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (env) => ({
+  entry: {
+    app: './src/index.tsx',
+    vendor: [
+      "react",
+      "react-dom",
+      "react-redux",
+      "react-router-dom",
+      "react-router-config",
+      "@better-scroll/core"
+    ]
+  },
+  output: {
+    filename: '[name].[hash:8].js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      "@": path.join(__dirname, "../src"),
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.(html)$/,
+      loader: "html-loader"
+    }, {
+      test: [/\.tsx$/, /\.ts$/],
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true
+        }
+      }
+    }, {
+      test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+      use: [{
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        }
+      }]
+    }, {
+      test: /\.(eot|woff2?|ttf|svg)$/,
+      use: [{
+        loader: 'url-loader',
+        options: {
+          name: '[name]-[hash:5].min.[ext]',
+          limit: 5000, // fonts file size <= 5KB, use 'base64'; else, output svg file
+          publicPath: 'fonts/',
+          outputPath: 'fonts/'
+        }
+      }]
+    }, {
+      test: /\.(scss|sass)$/,
+      use: [
+        env === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2,
+          }
+        },
+        'postcss-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  plugins: [
+  ],
+  optimization: {
+    runtimeChunk: true,
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+})
---
+++ build/webpack.dev.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) =>(merge(common(env), {
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: "../build",
+    port: 3000,
+    hot: true,
+    https: true,
+    public: 'www.amipei.xyz',
+    open: true,
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    key: fs.readFileSync(path.resolve(__dirname, '../amipei.xyz.key')),
+    cert: fs.readFileSync(path.resolve(__dirname, '../amipei.xyz.pem')),
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+  ]
+}))
