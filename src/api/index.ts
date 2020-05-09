c6440b74f4f936736fed6609f36a639a1587d8af
amipei <156516399@qq.com>
2020-05-09 10:56:59 +0800

    提交接口文件夹

---
+++ src/api/index.ts
@@ -0,0 +1,19 @@
+export {
+  sendSmsCodeRequest,
+  loginRequest,
+  logoutRequest,
+  getUserInfoRequest,
+  getUserTimerShaftRequest,
+  getUserLibraryCountRequest,
+  getUserFavesPressRequest,
+  getUserFavesAuthorRequest,
+  getSearchRequest,
+  getBookInfoRequest,
+  addBooksRequst
+} from './request'
+
+export {
+  getToken,
+  setToken,
+  removeToken
+} from './cookie'
